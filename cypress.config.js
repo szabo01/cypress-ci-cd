a823951d7f7347c172092dc2278a85864926970b
Robson Szabo <robsonszabo@yahoo.com.br>
2025-08-31 12:22:47 -0300

    Atualiza cypress.config.js com plugin do cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,21 @@
 const { defineConfig } = require("cypress");
 
-  module.exports = defineConfig({
-    reporter: 'cypress-mochawesome-reporter',
-    e2e: {
-      setupNodeEvents(on, config) {
-        return config;
-      },
-    },
-    reporterOptions: {
-      reportDir: 'cypress/reports/mochawesome-report',
-      reportFilename: 'mochawesome',
-      charts: true,
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      overwrite: true,
-      json: false
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome-report',
+    reportFilename: 'mochawesome',
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: true,
+    html: true,
+    json: true
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     }
-  });
+  }
+});
