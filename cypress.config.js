e283df24d9fb0f26d910441d3117db093a07bd30
Robson Szabo <robsonszabo@yahoo.com.br>
2025-09-05 16:13:41 -0300

    fix(config): switch to CLI-based reporter and plugin setup for mochawesome compatibility

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,9 @@
 module.exports = {
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome-report',
-    overwrite: false,
-    html: false,
-    json: true,
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false
-  },
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // Remova o require do plugin
+      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
-    }
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
   }
 };
