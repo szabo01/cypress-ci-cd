0409fd003ea761a5a97fa0ab6bb745972d53b1e0
Robson Szabo <robsonszabo@yahoo.com.br>
2025-09-05 11:11:03 -0300

    chore(config): register mochawesome plugin in Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,21 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
-  module.exports = defineConfig({
-    reporter: 'cypress-mochawesome-reporter',
-    e2e: {
-      setupNodeEvents(on, config) {
-        return config;
-      },
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  e2e: {
+    setupNodeEvents(on, config) {
+      mochawesome(on); // registra o plugin
+      return config;
     },
-    reporterOptions: {
-      reportDir: 'cypress/reports/mochawesome-report',
-      reportFilename: 'mochawesome',
-      charts: true,
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      overwrite: true,
-      json: false
-    }
-  });
+  },
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome-report",
+    reportFilename: "mochawesome",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: true,
+    json: false,
+  },
+});
