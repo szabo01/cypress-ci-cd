c333f72b8223a658c0c15f244a52bb1b692022b9
Robson Szabo <robsonszabo@yahoo.com.br>
2025-09-05 16:08:41 -0300

    fix(config): remove plugin require to allow mochawesome reporter to run in Cypress sandbox

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,20 @@
-const path = require('path');
-
 module.exports = {
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome-report',
+    overwrite: false,
+    html: false,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      require(path.join(__dirname, 'node_modules/cypress-mochawesome-reporter/plugin'))(on);
-      return config;
-    },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      reportDir: 'cypress/reports/mochawesome-report',
-      overwrite: false,
-      html: false,
-      json: true,
-      charts: true,
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false
+    setupNodeEvents(on, config) {
+      // Remova o require do plugin
+      return config;
     }
   }
 };
