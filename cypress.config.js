c95d3cdeb80361828a2fa2e97ffacf397753b5a3
Robson Szabo <robsonszabo@yahoo.com.br>
2025-09-05 16:06:26 -0300

    fix(config): use relative path to resolve mochawesome plugin from project directory

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,22 @@
+const path = require('path');
+
 module.exports = {
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require(path.join(__dirname, 'node_modules/cypress-mochawesome-reporter/plugin'))(on);
       return config;
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    // baseUrl: 'http://localhost:3000'
-  },
-  reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome-report',
-    reportFilename: 'mochawesome',
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    overwrite: true,
-    json: false
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome-report',
+      overwrite: false,
+      html: false,
+      json: true,
+      charts: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
   }
 };
