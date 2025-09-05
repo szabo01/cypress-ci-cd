0f7be81c470bf6d9e76525c8f5c32abf04a13387
Robson Szabo <robsonszabo@yahoo.com.br>
2025-09-05 15:14:15 -0300

    fix(config): resolve mochawesome plugin path for Docker compatibility

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 module.exports = {
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require(path.resolve('node_modules/cypress-mochawesome-reporter/plugin'))(on);
       return config;
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000'
   },
   reporterOptions: {
     reportDir: 'cypress/reports/mochawesome-report',
